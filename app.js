7e2f941f05dd883cf24751867d399bad1aa39fbe
Simon Königsreiter <simon.koenigsreiter@gmail.com>
2018-06-08 12:27:18 +0200

    Removed dynamic word fetching

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const osmosis = require('osmosis');
 const webpages = __dirname + '/static/';
+var words = {
+    Word: "Donaudampfschiffahrtskapitän",
+    Desc: "Kapitän eines Schiffes, das auf der Donau fährt",
+};
+// randomword.com
 
 app.get('/', (req, res) => {
     res.sendFile(webpages + 'index.html');
@@ -9,9 +15,19 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log("A user connected!");
+    
     socket.on('disconnect', () => {
         console.log("A user has disconnected!");
-    })
+    });
+    socket.on('input', (msg) => {
+        if (words.Word.includes(msg)) {
+            console.log(`User submitted ${msg} which was in the Word!`);
+            socket.emit('resp', true);
+        } else {
+            console.log(`User submitted ${msg} which was not in the Word!`);
+            socket.emit('resp', false);
+        }
+    });
 })
 
 http.listen(3000, function(){
